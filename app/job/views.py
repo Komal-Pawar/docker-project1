46e6ef861a94bcc80f1b6427957c747814b93823
Komal-Pawar <kasturipawar26@gmail.com>
2023-02-17 21:02:03 +0530

    detailview

--- app/job/views.py
+++ app/job/views.py
@@ -1,16 +1,23 @@
-"""Views for Job API"""
+"""Views for Job API
+TODO EXERCISE
+-------
+1. Create similar endpoints (list, detail) for `portal`
+2. Write as many test cases as you can for `portal` endpoints
+3. Create similar endpoints (list, detail) for `job_description`
+4. Write as many test cases as you can for `job_description` endpoints
+"""
 
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-## import models
+#  import models
 from core.models import JobTitle
 from job import serializers
 
 
 class JobTitleViewSet(viewsets.ModelViewSet):
-    serializer_class = serializers.JobTitleSerializer
+    serializer_class = serializers.JobTitleDetailSerializer
 
     # represents objects that are available for this viewset.
     # queryset objects that are manageable by this view.
@@ -27,3 +34,10 @@ class JobTitleViewSet(viewsets.ModelViewSet):
         """
 
         return self.queryset.filter(user=self.request.user).order_by("-id")
+
+    def get_serializer_class(self):
+        """Returns the serializer class to be used for the request"""
+
+        if self.action == "list":
+            return serializers.JobTitleSerializer
+        return self.serializer_class
